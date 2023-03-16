16d8c498a1e8f3e8ba25536f97aefb0b2ce33854
Lemanyaqubova <lamanny@code.edu.az>
2023-03-16 18:53:05 +0400

    firstAdd

---
+++ JuanProject/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace JuanProject.Models
+{
+    public class Comment
+    {
+        public int Id{ get; set; }
+        public string Message{ get; set; }
+        public DateTime CretedDate{ get; set; } 
+        public string AppUserId{ get; set; } 
+        public AppUser AppUser{ get; set; } 
+        public string BlogId { get; set; }
+        public Blog Blog  { get; set; }
+    }
+}
